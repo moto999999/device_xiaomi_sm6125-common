6e32378af0d3aece9a669106a9fb1584a1a44210
Pranav Vashi <neobuddy89@gmail.com>
2019-09-22 17:09:01 +0530

    sm6125-common: build 32-bit android.frameworks.displayservice@1.0
    Change-Id: I577b29ad7c006fe1408c4ced39b7de55388946db

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -141,7 +141,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0 \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
