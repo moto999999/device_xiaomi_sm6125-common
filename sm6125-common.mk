f694c691269ba9ffdc33cfa260e03d8507ec5a6d
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-07 08:36:07 -0600

    sm6125-common: add livedisplay 2.0 hal
    Change-Id: I1d5ee7c88d5ba2de57f963e748f2d34def042b3f

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -286,6 +286,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
