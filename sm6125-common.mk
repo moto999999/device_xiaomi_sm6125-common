891e0a829806821dcbe8a02284822965ef81e3a5
TARKZiM <tom8476oo@gmail.com>
2020-01-02 21:43:47 +0800

    Revert "violet: Drop Snap"
    This reverts commit fdf4a4b9c8429710cd35c32cc61c90446eeca404.

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -28,6 +28,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
@@ -142,7 +144,8 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    Snap
 
 # Context Hub
 PRODUCT_PACKAGES += \
